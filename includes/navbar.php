0f63c7fc5eb4e0314d33abdeebbfc90a3b99874d
seanwlk <nicola_97@rocketmail.com>
2024-02-19 20:51:08 +0100

    make top navbar profile trigged sidebar accordion

--- includes/navbar.php
+++ includes/navbar.php
@@ -7,7 +7,7 @@
     <div class="collapse navbar-collapse">
       <ul class="navbar-nav ms-auto me-0">
 
-        <li class="nav-item ">
+        <li class="nav-item " type="button" data-bs-toggle="collapse" data-bs-target="#profileAccordion">
           <span class="me-2"><?php echo isset($_SESSION['wfmb_username']) ? $_SESSION['wfmb_username'] : ''?></span>
             <img src="https://cdn.discordapp.com/avatars/<?php echo isset($_SESSION['wfmb_admin_dID']) ? $_SESSION['wfmb_admin_dID'] : ''?>/<?php echo isset($_SESSION['wfmb_avatar']) ? $_SESSION['wfmb_avatar'] : ''?>.png" class="p-0 rounded-circle" height="40px">
         </li>
